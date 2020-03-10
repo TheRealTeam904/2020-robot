ef020c6e44385a007656aa14b53c5f35286cf003
firstteam904 <firstteam904@gmail.com>
2020-03-09 20:04:27 -0400

    an apple a day

--- src/main/java/frc/robot/ShootSpeedAT.java
+++ src/main/java/frc/robot/ShootSpeedAT.java
@@ -27,7 +27,7 @@ public class ShootSpeedAT extends Command {
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
-    SmartDashboard.putNumber("ShootSpeedInAuto", SpeedOfAutoShoot);
+    Robot.shooter.ShootMotorSpeed(SpeedOfAutoShoot);
   }
 
   // Make this return true when this Command no longer needs to run execute()
